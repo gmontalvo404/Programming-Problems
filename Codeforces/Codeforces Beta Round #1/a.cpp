0470089fd509bbb13d5446375a21cdc350b29690
GMontalvo404 <gmontalvo404@gmail.com>
2020-03-11 23:34:20 -0500

    Error name folder and other proroblem

--- Codeforces/Codeforces Beta Round #1/a.cpp
+++ Codeforces/Codeforces Beta Round #1/a.cpp
@@ -2,20 +2,3 @@
 // Created by GMontalvo404 on 3/11/2020.
 //
 
-#include <bits/stdc++.h>
-#define endl '\n'
-using namespace std;
-
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    int w;
-    cin >> w;
-
-    if(w % 2 == 0 && w > 2) {
-        cout << "YES";
-    } else {
-        cout << "NO";
-    }
-}
