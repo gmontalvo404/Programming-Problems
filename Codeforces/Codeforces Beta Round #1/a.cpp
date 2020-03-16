76badad7a9908967325d1fadf7dcb1839993ab18
GMontalvo404 <gmontalvo404@gmail.com>
2020-03-15 19:51:42 -0500

    correction problem a CF #1

--- Codeforces/Codeforces Beta Round #1/a.cpp
+++ Codeforces/Codeforces Beta Round #1/a.cpp
@@ -1,4 +1,18 @@
-//
-// Created by GMontalvo404 on 3/11/2020.
-//
+#include <bits/stdc++.h>
+#define endl
+typedef long long ll;
+using namespace std;
 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    double n, m, a;
+    cin >> n >> m >> a;
+
+    int x = ceil(n / a);
+    int y = ceil(m / a);
+
+    ll z = (ll) x * y;
+    cout << z;
+}
