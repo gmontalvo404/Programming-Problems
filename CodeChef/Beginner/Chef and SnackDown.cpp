19a975ee5a46bb27fb079d09aa77208e261a9dbd
GMontalvo404 <gmontalvo404@gmail.com>
2020-04-16 22:08:20 -0500

    cc

---
+++ CodeChef/Beginner/Chef and SnackDown.cpp
@@ -0,0 +1,35 @@
+//https://www.codechef.com/problems/SNCKYEAR
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define rev(i, n, a) for (int i = n; i >= a; i--)
+#define ln '\n'
+#define space ' '
+#define cout(a, b) cout << a << space << b << ln;
+
+typedef long double ld;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<int> VI;
+typedef vector<ll> VLL;
+typedef vector<ull> VULL;
+typedef pair<int, int> PII;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t, n;
+    cin >> t;
+
+    while(t--) {
+        cin >> n;
+
+        if(n == 2010 || n == 2015 || n == 2016 || n == 2017 || n == 2019) cout << "HOSTED" << ln;
+        else cout << "NOT HOSTED" << ln;
+    }
+
+    return 0;
+}
